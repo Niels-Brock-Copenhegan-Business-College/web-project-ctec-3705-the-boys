9dbd3c4e99fde75f057d76c29c39aa540097281c
chitraranjan360 <chitraranjanyadavr360@gmail.com>
2026-07-04 00:20:21 +0200

    Remove sensitive email credentials from mail configuration

--- config/mail.php
+++ config/mail.php
@@ -2,9 +2,9 @@
 return [
     'host' => 'smtp.gmail.com',
     'port' => 587,
-    'username' => 'chitraranjanyadav2058@gmail.com',
-    'password' => 'xmuxaynhiqlghajp',
+    'username' => '', // Add your Gmail username here
+    'password' => '', // Add your Gmail password here
     'encryption' => 'tls',
-    'from_email' => 'chitraranjanyadav2058@gmail.com',
+    'from_email' => '', // Add your Gmail email here
     'from_name' => 'UK University Admissions',
 ];
